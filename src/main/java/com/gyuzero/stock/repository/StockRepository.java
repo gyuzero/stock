3eb2065f3c827250cc63aad640e6ee708f8c1f49
gyuzero <gyuzero0103@gmail.com>
2024-05-26 19:32:16 +0900

    Pessimistic Lock 활용해보기

--- src/main/java/com/gyuzero/stock/repository/StockRepository.java
+++ src/main/java/com/gyuzero/stock/repository/StockRepository.java
@@ -2,6 +2,15 @@ package com.gyuzero.stock.repository;
 
 import com.gyuzero.stock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.LockModeType;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Stock s WHERE s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
+
 }
---
+++ src/main/java/com/gyuzero/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,26 @@
+package com.gyuzero.stock.service;
+
+import com.gyuzero.stock.domain.Stock;
+import com.gyuzero.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+}
